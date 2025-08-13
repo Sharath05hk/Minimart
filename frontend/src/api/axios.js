b8935ccb977cb2e4db4da22cfef32c8eb94938ff
Sharath Raj H K <sharathrajhk05@gmail.com>
2025-08-13 16:23:28 +0530

    URL Changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://minimart-26gy.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
