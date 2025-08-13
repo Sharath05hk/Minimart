35192704f1533077d9bfeed258a5562a90c22453
Sharath Raj H K <sharathrajhk05@gmail.com>
2025-08-13 16:16:16 +0530

    URL Changed

--- backend/src/main/java/com/minimart/config/CorsConfig.java
+++ backend/src/main/java/com/minimart/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // frontend dev server
+                        .allowedOrigins("https://minimartfrontend.netlify.app") // frontend dev server
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
